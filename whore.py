926279b59b9a208cc292abc4feb9c53c518c1e90
Turtle <theturtle1983@gmail.com>
2013-05-07 01:44:19 +0700

    writing stub classes/methods

--- whore.py
+++ whore.py
@@ -3,22 +3,79 @@ try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
+from httplib2 import Http
+from datetime import datetime
+
 
 class Currency:
-	def __init__(self, conf):
-		self.symbol = conf["symbol"]
-		pass
+    def __init__(self, conf):
+        self.symbol = conf["symbol"]
+        pass
 
-	def __repr__(self):
-		return "Currency <%s>" %(self.symbol)
+    def get_difficulty(self):
+        """
+        Gets the current difficulty of this currency
+        """
+        pass
 
-	def __str__(self):
-		return "Currency <%s>" %(self.symbol)
+    def get_coin_per_day_hash(self):
+        """
+        How many coins can i get per hash?
+        """
+        pass
+
+    def __repr__(self):
+        return "Currency <%s>" %(self.symbol)
+
+    def __str__(self):
+        return "Currency <%s>" %(self.symbol)
+
+class Exchange:
+    """
+    Class for currency conversions.
+    Cache results for future
+    """
+    def __init__(self):
+        self.h = Http() #Initialize here to keep conn pool
+        self.cache = {} #Cache obj
+
+    def convert(self, symbol):
+        """
+        1 of this currency gets me how many BTC?
+        """
+        cached = self.cache.get(symbol)
+        if cached is None or (datetime.now() - cached["updated"]).total_seconds > 300  :
+            #I.e. cache doesnt exist or cache is more than 5 min stale
+            result = self._convert(symbol)
+            self.cache[symbol] = {
+                "rate": result,
+                "updated": datetime.now()
+            }
+        else:
+            result = cached["rate"]
+        return result
+
+    def _convert(self, symbol):
+        """
+        Do actual lookup at exchange
+        """
+        if symbol == "BTC":
+            return 1.0
+        else:
+            pass #TODO
+
+class Miner:
+    """
+    Model representing each individual mining unit
+    """
+    def __init__(self, config):
+        pass
+        
 
 if "__main__" in __name__ :
-	#Load the config
-	config = load(open("config.yaml"), Loader=Loader)
-	curriencies = {}
-	for coin in config["coins"]:
-		curriencies[coin["symbol"]] = Currency(coin)
-	print curriencies
+    #Load the config
+    config = load(open("config.yaml"), Loader=Loader)
+    curriencies = {}
+    for coin in config["coins"]:
+        curriencies[coin["symbol"]] = Currency(coin)
+    print curriencies
